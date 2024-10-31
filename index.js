02d47906ec4d43e320307488e25a0e21888fcdb3
unknown <petra.j-son@outlook.com>
2024-10-31 13:34:11 +0100

    First 3  excersises functions

---
+++ index.js
@@ -0,0 +1,23 @@
+// Uppgift 1
+
+function greeting (){
+    console.log("Hello world")
+}
+
+// Uppgift 2
+
+function favoriteNummber (){
+    console.log("My favorite nummber is 42")
+}
+
+// Uppgift 3
+
+function magicEightBall(){
+    const options = ["Maybe","Ask again later","yes","no"];
+    const randomNr = Math.floor(Math.random() * options.length);
+    console.log(options[randomNr])
+}
+
+magicEightBall();
+
+// Uppgift 4
